df84a597c8f76db25ee1c6b4d6473583fa2ec682
mrdoomus <mrdoom.official@gmail.com>
2020-02-14 16:40:37 -0500

    features: [Update] Red cricle on API status code 200

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,11 @@
+$(function (){
+  $.ajax ({
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    statusCode: {
+      200: function () {
+        $('div#api_status').addClass('available');
+      }
+    }
+  });
+});
